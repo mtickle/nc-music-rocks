a7eb1b489ba06387d91b8db0952c4d893ad03000
Mike Tickle <mtickle@gmail.com>
2020-11-05 14:39:06 +0000

    Added db_connect

---
+++ db_connect.php
@@ -0,0 +1,8 @@
+<?php
+$dbhost = 'mkorvuw3sl6cu9ms.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';  
+$dbuser = 'l1ulm9n6lxu8v7om';  
+$dbpass = "tiswm9ms7y3lq4x5";  
+$conn = mysqli_connect($dbhost, $dbuser, $dbpass) or die ('Error connecting to mysql');  
+$dbname = 'gj75npjq39icxbe1';  
+$connection = mysqli_select_db($conn, $dbname); 
+?>
