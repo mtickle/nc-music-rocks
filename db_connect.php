e8bd0af5e7b2ab67d0a1fbca1e0629342a8bf57e
Mike Tickle <mtickle@gmail.com>
2020-11-07 14:22:32 +0000

    i hate you

--- db_connect.php
+++ db_connect.php
@@ -1,6 +1,5 @@
 <?php
 
-
 $hostname =  gethostname();
 
 if ($hostname=="raspberrypi") {
@@ -15,29 +14,7 @@ if ($hostname=="raspberrypi") {
     $edbname = getenv("nmr_db_database"); 
 }
 
-
-// require __DIR__ . '/vendor/autoload.php';
-// $dotenv = Dotenv\Dotenv::createImmutable(__DIR__.'/');
-
-// if(file_exists(".env")) {
-//     $dotenv->load();
-//     $edbhost = $_ENV['nmr_db_host'];
-//     $edbuser = $_ENV['nmr_db_user'];
-//     $edbpass = $_ENV["nmr_db_password"]; 
-//     $edbname = $_ENV["nmr_db_database"]; 
-// } else {
-//     $edbhost = getenv('nmr_db_host');
-//     $edbuser = getenv('nmr_db_user');
-//     $edbpass = getenv("nmr_db_password"); 
-//     $edbname = getenv("nmr_db_database"); 
-// };
-
- echo $hostname . " - " . $edbhost . " - " . $edbuser . " - " . $edbpass . " - " . $edbname;
-
-
+// echo $hostname . " - " . $edbhost . " - " . $edbuser . " - " . $edbpass . " - " . $edbname;
 $conn = mysqli_connect($edbhost, $edbuser, $edbpass) or die ('Error connecting to mysql');  
 $connection = mysqli_select_db($conn, $edbname); 
-
-
-
 ?>
